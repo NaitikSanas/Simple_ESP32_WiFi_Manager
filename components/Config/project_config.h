74e3ffce83cc8dd259080d523b1f6276bad23ffc
Naitik <33337376+NaitikSanas@users.noreply.github.com>
2023-07-18 11:46:13 +0530

    Update project_config.h

--- components/Config/project_config.h
+++ components/Config/project_config.h
@@ -2,7 +2,7 @@
 #define BTN_SET_WIFI    0
 
 /****** WIFI Manager specifics ********/
-#define USE_DNS_SERVER
+#define USE_DNS_SERVER 0
 #define AP_SSID     "WIFI_MANAGER"
 #define AP_PASSWORD "wifi_setup"
 #define AP_MAX_CONN  1
